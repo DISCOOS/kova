978f459e8d5853cd631a3116c23e34fe7eb942a3
roarfred <roarfred@gmail.com>
2017-02-15 23:03:23 +0100

    Removed connection string from context class

---
+++ kova.api/Models.partial/kovaContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kova.api.Models
+{
+    public partial class kovaContext
+    {
+        public kovaContext(DbContextOptions<kovaContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- kova.api/Models/kovaContext.cs
+++ kova.api/Models/kovaContext.cs
@@ -82,8 +82,6 @@ namespace kova.api.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=kova;Trusted_Connection=True");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
