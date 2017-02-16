fd3614771897ff616fd01c64e68ce48370ac8d41
roarfred <roarfred@gmail.com>
2017-02-16 22:26:50 +0100

    Method for setting password

--- kova.api/Models.partial/TOrganizationPerson.cs
+++ kova.api/Models.partial/TOrganizationPerson.cs
@@ -26,5 +26,30 @@ namespace kova.api.Models
 
             return calculatedHash.SequenceEqual(hashBinary);
         }
+
+        public void SetPassword(string password)
+        {
+            var salt = CreateSalt();
+
+            var passwordArray = System.Text.Encoding.UTF8.GetBytes(password);
+
+            byte[] buffer = new byte[salt.Length + passwordArray.Length];
+            Array.Copy(salt, buffer, salt.Length);
+            Array.Copy(passwordArray, 0, buffer, salt.Length, passwordArray.Length);
+
+            var hasher = new System.Security.Cryptography.SHA256Managed();
+            var hash = hasher.ComputeHash(passwordArray);
+
+            this.Salt = Convert.ToBase64String(salt);
+            this.Hash = Convert.ToBase64String(hash);
+        }
+
+        private static byte[] CreateSalt()
+        {
+            byte[] buffer = new byte[20];
+            var randomizer = new System.Security.Cryptography.RNGCryptoServiceProvider();
+            randomizer.GetBytes(buffer);
+            return buffer;
+        }
     }
 }
